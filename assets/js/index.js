6a93a4323f02ed1a72512f852140e5336886f65e
muhammad ismail <muhismailmks@gmail.com>
2024-06-08 10:07:04 +0800

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,28 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll(".section_links");
+    const navLinks = document.querySelectorAll(".menu-list a");
+
+    window.addEventListener("scroll", () => {
+        let current = ""; 
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (pageYOffset >= sectionTop - sectionHeight / 4) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").substring(1) === current) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
 
 // copy address
 const copybtns = document.querySelectorAll(".copy_address");
