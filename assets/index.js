49507548b05e21c3c841801dd60d3bb2e1abbd6a
muhammad ismail <muhismailmks@gmail.com>
2025-09-17 14:11:49 +0800

    change to money machine

--- assets/index.js
+++ assets/index.js
@@ -3,45 +3,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     AOS.init({
         once: true,
-    }); 
+    });
 
+    // menu navbar
+    const menu = document.querySelector('.menu');
+    const menuBtn = document.querySelector('.menu-button');
+    const menuLinks = document.querySelectorAll('.menu ul li a'); // ambil semua link dalam menu
 
-    // star animation
-    const container = document.querySelector(".stars-container");
-    const starImage = "./assets/images/star.png"; 
- 
-   const starCount = window.innerWidth < 1000 ? 15 : 30;
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('hidden');
+    });
 
-   for (let i = 0; i < starCount; i++) { 
-       let star = document.createElement("img");
-       star.src = starImage;
-       star.classList.add("star");
-
-       let x = Math.random() * window.innerWidth;
-       let y = Math.random() * window.innerHeight;
-
-       let size = Math.random() * 30 + 5; 
-       let duration = Math.random() * 2 + 1;
-
-       star.style.width = `${size}px`;
-       star.style.height = `${size}px`;
-       star.style.left = `${x}px`;
-       star.style.top = `${y}px`;
-       star.style.animationDuration = `${duration}s`;
-
-       container.appendChild(star);
-   }
-
-
-
-    //    memes slider
-    const items = document.querySelectorAll('.wrapper .item');
-    const totalItems = items.length; 
-    const animationDuration = 30;  
-
-    items.forEach((item, i) => { 
-        const delay = (animationDuration / totalItems) * (totalItems - i) * -1;
-        item.style.animationDelay = `${delay}s`;
+    // kalau klik link di dalam menu -> close menu
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menuBtn.classList.remove('active');
+            menu.classList.add('hidden');
+        });
     });
 
 
